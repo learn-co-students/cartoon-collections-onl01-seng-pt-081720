086c8455ec47da9ee67e286c0baa2d9e4a6d6240
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-26 20:08:50 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(dwarves)
+  i = 0 
+  while i < dwarves.length 
+    print "#{i + 1}. #{dwarves[i]}"
+    i += 1 
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+   x = [ ]
+  planeteer_calls.map do |i|
+   x = i.capitalize + "!"
+  end 
+end 
+
+short_words = ["puff", "go", "two"] 
+
+def long_planeteer_calls(short_words)
+  i = 0 
+  if  short_words.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+snacks =  ["crackers","gouda","thyme"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do  |x|
+  cheese_types.include?(x)
+end 
 end
+
+
