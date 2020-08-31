446ca55ed085f7fa9e51f0ee4357e0a501e34d9e
Myles Janik <mylesjanik@gmail.com>
2020-08-31 00:53:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,14 @@ def summon_captain_planet(array)# code an argument here
   new_array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
-  
+  array.each do |calls|
+    if calls.length > 4
+      return true
+    else
+      return false
+  end
 end
 
 def find_the_cheese# code an argument here
