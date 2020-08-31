9696dc855e94e71fd4a438dbde393c639bde0492
Myles Janik <mylesjanik@gmail.com>
2020-08-31 00:50:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@ def summon_captain_planet(array)# code an argument here
   new_array = []
   
   array.each do |element|
-    
+    new_array << element.capitalize
   end
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
