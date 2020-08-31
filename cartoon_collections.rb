82d5700996c4a80e28de20ce0fb34764acfa0e18
Myles Janik <mylesjanik@gmail.com>
2020-08-31 00:49:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,13 @@ def roll_call_dwarves(names)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  new_array = []
   
+  array.each do |element|
+    
+  end
 end
 
 def long_planeteer_calls# code an argument here
